4817a577f851ec7b6e22199ffd3c44c7ad0e603d
Brent Mellor <brentmellor@gmail.com>
2017-02-18 03:57:18 -0700

    Done with text of I2C lab. Probably want to add an appendix or section with background information on the gyro or temperature sensors they will be communicating with.

--- Lab5/Files/setup.c
+++ Lab5/Files/setup.c
@@ -1,13 +1,11 @@
 /* Clear the NBYTES and SADD bit fields
- * The NBYTES field begins at bit 16
- * The SADD field begins at bit 0
+ * The NBYTES field begins at bit 16, the SADD at bit 0
  */
-I2C1->CR2 &= ~((0x7F << 16) | (0x3FF << 0));
+I2C2->CR2 &= ~((0x7F << 16) | (0x3FF << 0));
 
-/* Set the NBYTES and SADD bit fields
+/* Set NBYTES = 42 and SADD = 0x14
  * Can use hex or decimal values directly as bitmasks. 
- * Sets NBYTES = 42, and SADD = 0x14
  * Remember that for 7-bit addresses, the lowest SADD bit
- * 	 is not used and the mask must be shifted by one.
+ * is not used and the mask must be shifted by one.
  */
-I2C1->CR2 &= ~((42 << 16) | (0x14 << 1));
+I2C2->CR2 &= ~((42 << 16) | (0x14 << 1));
